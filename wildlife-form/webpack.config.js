a5cc8e352a665a1ce5011ce2c12e860e88bd3f18
pharingWell <30706897+pharingWell@users.noreply.github.com>
2024-08-13 20:12:38 -0400

    Excluded modules, updated dependencies, added webpack config

---
+++ wildlife-form/webpack.config.js
@@ -0,0 +1,10 @@
+
+//  import 'bootstrap/dist/css/bootstrap.min.css';
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    }
+};
