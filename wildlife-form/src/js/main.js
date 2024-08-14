1450a8093191e95dfa7de41637e594c37a9f0809
pharingWell <30706897+pharingWell@users.noreply.github.com>
2024-08-13 20:48:38 -0400

    We have liftoff!
    Turns out, it was loading the unpacked bundle.js from src, which doesn't exist. Oops

--- wildlife-form/src/js/main.js
+++ wildlife-form/src/js/main.js
@@ -1,2 +1,2 @@
-import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { content } from './module';
--- wildlife-form/webpack.config.js
+++ wildlife-form/webpack.config.js
@@ -1,10 +1,19 @@
-
-//  import 'bootstrap/dist/css/bootstrap.min.css';
+const path = require("path");
 
 module.exports = {
     entry: './src/js/main.js',
     output: {
-        path: __dirname + '/dist',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: path.resolve(__dirname, 'dist', 'bundle.js'),
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            // CSS rules
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
