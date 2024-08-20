941b6a40a22acc4387270ffcf6eed0434f076e18
pharingWell <30706897+pharingWell@users.noreply.github.com>
2024-08-19 23:23:28 -0400

    the second part of the commit

--- src/js/form.js
+++ src/js/form.js
@@ -1,23 +1,4 @@
-import $ from 'jquery';
-import {html} from "jquery";
-
 export function initialize() {
-    // $.getJSON('http://anyorigin.com/get?url=http://docs.google.com/forms/d/e/1FAIpQLSc1HS1p1aGzZVWm53Mp0hGclQ4hvlUu0R8WxgIRS1k9zvr1Wg/viewform?embedded=true&callback=?', function(data){
-    //     console.log(data);
-    //     //$('#output').html(data.contents);
-    // });
 
-    let url = "https://docs.google.com/forms/d/e/1FAIpQLSc1HS1p1aGzZVWm53Mp0hGclQ4hvlUu0R8WxgIRS1k9zvr1Wg/viewform";
-    let out = document.getElementById("out");
-    fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
-        .then(response => {
-            if (response.ok) return response.json()
-            throw new Error('Network response was not ok.')
-        })
-        .then(data => {
-            console.log(data.contents);
-            const htmlOut = $.parseHTML(data.contents.html);
-            $("out").html(htmlOut)
-        });
 }
 
