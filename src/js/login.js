9143cebe28f0fd835624a95405fd4fee17f90fc7
pharingWell <30706897+pharingWell@users.noreply.github.com>
2024-08-20 00:53:27 -0400

    login should now automatically redirect

--- src/js/login.js
+++ src/js/login.js
@@ -2,11 +2,6 @@ import ZOHO_CLIENT_ID from '/dist/zoho.js'
 const REDIRECT_URI = "https://pharingwell.github.io/wildlife_collisions/form"
 const STATE = "Aw73?&tAKL4394"
 function initialize() {
-    let xmlHttp = new XMLHttpRequest();
-
-    function callback(responseText) {
-        console.log(responseText);
-    }
     let zohoEndpoint = `https://accounts.zoho.com/oauth/v2/auth`+
         `?client_id=${ZOHO_CLIENT_ID}`+
         `&response_type=token`+
@@ -16,13 +11,7 @@ function initialize() {
     ;
     let srcReplace = document.getElementsByClassName("fallback")[0];
     srcReplace.setAttribute('href', zohoEndpoint);
-    console.log(zohoEndpoint);
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", zohoEndpoint, true); // true for asynchronous
-    xmlHttp.send(null);
+    window.location.href = zohoEndpoint;
 }
 
 initialize();
