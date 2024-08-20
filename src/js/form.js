f3f4c43e3418da948be4200bdb10ee0a7fb8a094
pharingWell <30706897+pharingWell@users.noreply.github.com>
2024-08-20 00:25:51 -0400

    attempt at zoho get

--- src/js/form.js
+++ src/js/form.js
@@ -1,4 +1,24 @@
+import ZOHO_CLIENT_ID from '/dist/zoho.js'
+const REDIRECT_URI = "https://pharingwell.github.io/wildlife_collisions/redirect"
+const STATE = "A#73*&tA^KL4$394"
 export function initialize() {
-    
+    let xmlHttp = new XMLHttpRequest();
+
+    function callback(responseText) {
+        console.log(responseText);
+    }
+    let zohoEndpoint = `https://accounts.zoho.com/oauth/v2/auth
+        ?client_id=${ZOHO_CLIENT_ID}
+        &response_type=token
+        &redirect_uri=${REDIRECT_URI}
+        &scope=ZohoSheet.dataAPI.UPDATE
+        &state=${STATE}
+    `;
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", zohoEndpoint, true); // true for asynchronous
+    xmlHttp.send(null);
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { LinkedEntry } from './types';
 import { initialize } from './form';
-import ZOHO_CLIENT_ID from '/dist/zoho.js'
 
 initialize();
-console.log(ZOHO_CLIENT_ID);
