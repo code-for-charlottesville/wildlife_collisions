f3ff181a023b7ace243c0e2e6d25a4b1a4a6fa49
pharingWell <30706897+pharingWell@users.noreply.github.com>
2024-08-20 00:09:13 -0400

    zoho test 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { LinkedEntry } from './types';
 import { initialize } from './form';
+import ZOHO_CLIENT_ID from '/dist/zoho.js'
 
 initialize();
+console.log(ZOHO_CLIENT_ID);
