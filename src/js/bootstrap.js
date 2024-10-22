e5918c8862ecb7490aaaed1c42252f1e4194a61b
pharingWell <30706897+pharingWell@users.noreply.github.com>
2024-10-22 19:35:17 -0400

    Wizard as of demo

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "@popperjs/core";
 import 'bootstrap/dist/js/bootstrap.js';
 // do not delete this file
 // it provides the website with bootstrap
---
+++ src/js/wizard.js
@@ -0,0 +1,3 @@
+import {testEntries} from "./config";
+
+
