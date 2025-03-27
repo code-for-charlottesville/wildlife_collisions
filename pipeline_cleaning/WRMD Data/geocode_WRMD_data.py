1cad85dc057b23889be21102551fa1f55d3909ad
Clark Pagels <Cjpagels2@gmail.com>
2025-03-27 14:57:06 -0400

    virginia
    checking to see if location is in virginia.

--- pipeline_cleaning/WRMD Data/geocode_WRMD_data.py
+++ pipeline_cleaning/WRMD Data/geocode_WRMD_data.py
@@ -2,8 +2,31 @@
 import pandas as pd
 import geopy
 from geopy.geocoders import Nominatim
+from shapely.geometry import Point
+# %%
+# Download Virginia state boundary
+# TIGER/Line 2023 state boundaries
+url = "https://www2.census.gov/geo/tiger/TIGER2023/STATE/tl_2023_us_state.zip"
 # %%
 
+# Load into GeoDataFrame
+states = gpd.read_file(url)
+# %%
+
+# Filter for Virginia
+virginia = states[states.NAME == 'Virginia']
+# %%
+
+# Save to local file if you want to reuse
+virginia.to_file("virginia_boundary.shp")  # or .geojson, .gpkg
+
+# %%
+#virginia checker function 
+def is_in_virginia(lat, lon, virginia_gdf):
+    point = Point(lon, lat)
+    return virginia_gdf.geometry.contains(point).any()
+
+#%%
 # Load the data that needs geocoding
 df = pd.read_pickle('./datasets/WRMD_2014_to_2025_geocoding_req.pkl')
 # %%
@@ -17,4 +40,11 @@ geolocator = Nominatim(user_agent="WildVirginia")
 test_location = geolocator.geocode(f'{test_address}, {test_city}')
 # %%
 
+
+#latidute and longitude 
+latitude = test_location.latitude
+longitude = test_location.longitude
+
+# %%
 # Validate that the returned geocoded location is WITHIN VA
+valitadion = is_in_virginia(latitude,longitude,virginia)
